24765f730ff7d51267d40d8f0809b4782b1360e6
Rayan Hossain <hossain_shohan1@live.com>
2024-04-17 14:59:50 +0600

    world_country list seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,7 +14,8 @@ class DatabaseSeeder extends Seeder
     {
         $this->call([
             // SettingSeeder::class,
-            RolePermissionSeeder::class,
+            // RolePermissionSeeder::class,
+            WorldCountrySeeder::class,
         ]);
     }
 }
